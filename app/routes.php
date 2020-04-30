e1ae63630db9633bbb485656d3d6d925c6abe7e4
fattahi <ff@fft.com>
2020-04-30 20:33:31 +0430

    t

--- app/routes.php
+++ app/routes.php
@@ -32,7 +32,7 @@ $app->group('/profportfolio/api/v1', function () {
 // Define app routes
 $app->get('/', function ($request, $response, $args) {
     //echo $_SERVER['DOCUMENT_ROOT'];
-        return $response->write("Hello***** " );
+        return $response->write("Hello 1234***** " );
 });
 $app->get('/super', function ($request, $response, $args) {
     //echo $_SERVER['DOCUMENT_ROOT'];
@@ -41,6 +41,6 @@ $app->get('/super', function ($request, $response, $args) {
 
 $app->get('/helo/{name}', function($request, $response, $args) {
 
-    return $response->write( "Hello, " . $args['name']);
+    return $response->write( "Hello, 111 " . $args['name']);
 });
 
