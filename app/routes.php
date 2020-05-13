91c017d7cb590cdd1dc7cab0350c7f3eefd781e7
fattahi <ff@fft.com>
2020-05-13 21:37:32 +0430

    t

--- app/routes.php
+++ app/routes.php
@@ -14,7 +14,7 @@ use Api\Controllers\FormCalendarsController;
 
 $app->group('/profportfolio/api/v1', function () {
 	
-    //$this->post('/test/insert', SuperGroupsController::class . ':testinsert')->setName('SuperGroups.testinsert'); 
+    $this->post('/test/insert', SuperGroupsController::class . ':testinsert')->setName('SuperGroups.testinsert'); 
     $this->get('/supergroups/select/{id}', SuperGroupsController::class . ':select')->setName('SuperGroups.select');
     $this->get('/supergroups/selectAll', SuperGroupsController::class . ':selectAll')->setName('SuperGroups.selectAll');
     /*$this->post('/supergroups/select[/{params:.*}]', SuperGroupsController::class . ':select')->setName('SuperGroups.select');// params is the optional query parameters for selection*/
