bcc16d41f5ac1eaae66806799a4b79c28ad936b2
jafarkhani <jafarkhani@172.21.13.12>
2020-11-21 02:36:25 +0330

    Merge origin/master

--- app/routes.php
+++ app/routes.php
@@ -16,23 +16,6 @@ $app->group('/api/v1', function () {
     $this->post('/groups/update', GroupsController::class . ':update')->setName('Groups.update');
     $this->delete('/groups/delete/{id}', GroupsController::class . ':delete')->setName('Groups.delete');
 
-    $this->get('/formtypes/select/{id}', FormTypesController::class . ':select')->setName('FormTypes.select');
-    $this->get('/formtypes/selectAll', FormTypesController::class . ':selectAll')->setName('FormTypes.selectAll');
-    $this->post('/formtypes/insert', FormTypesController::class . ':insert')->setName('FormTypes.insert');
-    $this->post('/formtypes/update', FormTypesController::class . ':update')->setName('FormTypes.update');
-    $this->delete('/formtypes/delete/{id}', FormTypesController::class . ':delete')->setName('FormTypes.delete');
-
-    $this->get('/items/select/{id}', ItemsController::class . ':select')->setName('Items.select');
-    $this->get('/items/selectAll', ItemsController::class . ':selectAll')->setName('Items.selectAll');
-    $this->post('/items/insert', ItemsController::class . ':insert')->setName('Items.insert');
-    $this->post('/items/update', ItemsController::class . ':update')->setName('Items.update');
-    $this->delete('/items/delete/{id}', ItemsController::class . ':delete')->setName('Items.delete');
-
-    $this->get('/formcalendars/select/{id}', FormCalendarsController::class . ':select')->setName('FormCalendars.select');
-    $this->get('/formcalendars/selectAll', FormCalendarsController::class . ':selectAll')->setName('FormCalendars.selectAll');
-    $this->post('/formcalendars/insert', FormCalendarsController::class . ':insert')->setName('FormCalendars.insert');
-    $this->post('/formcalendars/update', FormCalendarsController::class . ':update')->setName('FormCalendars.update');
-    $this->delete('/formcalendars/delete/{id}', FormCalendarsController::class . ':delete')->setName('FormCalendars.delete');
 });
 
 /*$app->delete('/profportfolio/api/v1/supergroups/delete/{id}', function ($request, $response, $args) {
