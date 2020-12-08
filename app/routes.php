b7e1052354fc26cbfcf02e16c11de76aad2b72bb
jafarkhani <jafarkhani@172.21.13.12>
2020-12-08 12:10:50 +0330

    Merge origin/master
    Conflicts:
    	config.php

--- app/routes.php
+++ app/routes.php
@@ -17,28 +17,6 @@ $app->group('/api/v1', function () {
 	
 	$this->get('/FormHeader/selectAll', 'ProfPortfolio\Controllers\FormHeaderController:selectAll');
 	$this->post('/FormHeader/save', 'ProfPortfolio\Controllers\FormHeaderController:save');
-	
-    
+	$this->get('/FormHeader/compute/{id}', 'ProfPortfolio\Controllers\FormHeaderController:compute');
 
 });
-
-/*$app->delete('/profportfolio/api/v1/supergroups/delete/{id}', function ($request, $response, $args) {
-    //echo $_SERVER['DOCUMENT_ROOT'];
-        return $response->write("delete 1234***** ".$args['id']);
-});*/
-
-// Define app routes
-$app->get('/', function ($request, $response, $args) {
-    //echo $_SERVER['DOCUMENT_ROOT'];
-        return $response->write("Hello 1234***** ");
-});
-$app->get('/test', function ($request, $response, $args) {
-    //echo $_SERVER['DOCUMENT_ROOT'];
-        return $response->write(" test " );
-});
-
-$app->get('/helo/{name}', function($request, $response, $args) {
-
-    return $response->write( "Hello, 111 " . $args['name']);
-});
-
