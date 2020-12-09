969f56c1e7c4fb0858676f0c59c35a0df9799aa4
jafarkhani <jafarkhani@172.21.13.12>
2020-12-09 12:53:38 +0330

    Merge origin/master
    Conflicts:
    	config.php

--- app/dependencies.php
+++ app/dependencies.php
@@ -32,8 +32,8 @@ $container['errorHandler'] = function ($container) {
 		
         $response = $container['response'];
 		
-		return $response->withJson(['errors' => $errorDesc], 200);
+		//return $response->withJson(['errors' => $errorDesc], 200);
 		
-        //return $response->withJson(['errors' => 'Something went wrong!'], 500);
+        return $response->withJson(['errors' => 'Something went wrong!'], 500);
     };
 };
