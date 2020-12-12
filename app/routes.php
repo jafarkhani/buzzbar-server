6fda5d26b18fc1ff65d732dde2b209e406e22007
jafarkhani <jafarkhani@172.21.13.12>
2020-12-12 15:26:48 +0330

    Merge origin/master
    Conflicts:
    	composer.json

--- app/routes.php
+++ app/routes.php
@@ -22,7 +22,8 @@ $app->group('/api/v1', function () {
 	
 	$this->get('/FormHeader/selectAll', 'ProfPortfolio\Controllers\FormHeaderController:selectAll');
 	$this->post('/FormHeader/save', 'ProfPortfolio\Controllers\FormHeaderController:save');
-	$this->get('/FormHeader/compute/{id}', 'ProfPortfolio\Controllers\FormHeaderController:compute');
+	$this->get('/FormHeader/compute', 'ProfPortfolio\Controllers\FormHeaderController:compute');
+	$this->get('/FormHeader/IndicatorCompute', 'ProfPortfolio\Controllers\FormHeaderController:IndicatorCompute');
 
 	$this->get('/Reports/Report_FormDetails', 'ProfPortfolio\Controllers\FormHeaderController:Report_FormDetails');
 	$this->get('/Reports/Report_PersonDetails', 'ProfPortfolio\Controllers\FormHeaderController:Report_PersonDetails');
